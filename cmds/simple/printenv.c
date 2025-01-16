9a458fffa49a82420136e87ed45caa0e318b5835
Warren Toomey <wkt@tuhs.org>
2025-01-16 15:48:03 +1000

    cmds/simple/printenv.c: Get `printenv` to work, fix the `v7sh` Makefile.

--- cmds/simple/printenv.c
+++ cmds/simple/printenv.c
@@ -10,6 +10,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Because xv6 doesn't set the environ, we simulate it here
+#define ENVLEN 100
+#define ENV_FILE "/etc/env"
+#define ENV_LINE_LEN 200
+
+static char * xv6env[ENVLEN];
+extern char **environ;
+
+void loadxv6env()
+{
+  FILE *in;
+  char env_line[ENV_LINE_LEN];
+  int posn=0;
+
+    // Try to open the env file
+  if ((in=fopen(ENV_FILE, "r"))==NULL) return;
+
+  while (fgets(env_line, ENV_LINE_LEN-1, in)!=NULL) {
+    // Ignore comments
+    if (env_line[0] == '#') continue;
+
+    // Stop if we run out of room
+    if (posn==ENVLEN) break;
+
+    // Remove the newline
+    env_line [ strlen(env_line) - 1] = '\0';
+
+    // Copy the variable into the array
+    xv6env[posn++]=strdup(env_line);
+  }
+  fclose(in);
+  environ= xv6env;
+  return;
+}
+
+
 void writesnl(const char *p)
 {
     write(1, p, strlen(p));
@@ -21,6 +57,8 @@ int main(int argc, char *argv[])
     char **env;
     extern char **environ;
     int len;
+
+    loadxv6env();
 
     env = environ;
 
